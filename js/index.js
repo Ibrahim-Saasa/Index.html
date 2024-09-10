cc75d90bca93ed54578326e612667cb9867949cb
Ausar-Vile <ibrahimsaasa@gmail.com>
2024-09-10 22:16:17 +0400

    final changes

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ checkbox.addEventListener("change",()=>{
 if (checkbox.checked) {
     document.body.style.color = "white";
     document.documentElement.style.setProperty("--my-gradient", "black");
-  } else {
+  } 
+  if (checkbox.unchecked) {
     document.body.style.color = "white";
     document.documentElement.style.setProperty("black", "--my-gradient");
   }
