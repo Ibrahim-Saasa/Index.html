f0b59139ac20fbac60cf16a5314e0f0c3aa93a7e
ibrahimsaasa@gmail.com <ibrahimsaasa@gmail.com>
2024-09-11 10:57:33 +0400

    updated mod-theme

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-let checkbox = document.getElementById("mod-theme");
+let darkmode = localStorage.getItem('darkmode')
+const checkbox = document.getElementById("mod-theme");
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDrakmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
 checkbox.addEventListener("change",()=>{
-if (checkbox.checked) {
-    document.body.style.color = "white";
-    document.documentElement.style.setProperty("--my-gradient", "black");
-  } else {
-    document.body.style.color = "white";
-    document.documentElement.style.setProperty("black", "--my-gradient");
-  }
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDrakmode()
+// if (checkbox.checked) {
+//     document.body.style.color = "white";
+//     document.documentElement.style.setProperty("--my-gradient", "black");
+//   } else {
+//     document.body.style.color = "white";
+//     document.documentElement.style.setProperty("black", "--my-gradient");
+//   }
 }) 
