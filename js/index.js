b266c732cd180325038c34c4e716bc09c165338d
Ausar-Vile <ibrahimsaasa@gmail.com>
2024-09-21 23:12:18 +0400

    added path to buttons

--- js/index.js
+++ js/index.js
@@ -41,3 +41,18 @@ closeBtn.addEventListener("click", () => {
   burgerBtn.style.display = "block";
   closeBtn.style.display = "none";
 });
+
+function sendToWhattsapp() {
+  let number = "+971554296952"
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+
+  var url = "https:wa.me/" + number + "?text=" 
+  + "Name : " +name+ "%0a"
+  + "Email : " +email+ "%0a"
+  + "Message : " +message+ "%0a%0a"
+
+  window.open(url, '_blank').focus();
+}
